e99a5a096ad2996cf07b71fcd2831f5d0b12237c
Vatsal Garg <106297224+vgarg1234@users.noreply.github.com>
2023-11-23 18:27:32 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,25 @@
+//Array:- Array are the collection of the datatypes.
+
+var a =10;
+var b =20;
+
+console.log(a);
+
+var arr=[230,38,70,"good" ,"fool"]
+console.log[arr];
+
+arr.push(20);
+console.log(arr);
+
+arr.pop(30);
+console.log(arr);
+
+arr.shift(20);
+console.log(arr);
+
+arr.unshift(150);
+console.log(arr);
+
+arr.splice(3,4,"vatsal");
+console.log(arr);
+
